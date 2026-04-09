72c36c64c30f49e78a2d5950d477015a1506a1b2
yashrevankar26-ai <yashrevankar26@gmail.com>
2026-04-09 13:38:16 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -3,7 +3,7 @@ select * from pizza_sales;
 
 
 #KPI     
-Select sum(total_price) as Total_Revenue from pizza_sales;
+Select sum(total_price) as Total_Revenue from pizza_sales; 
 
 Select sum(total_price) / count(distinct order_id) as Avg_Order_Value from pizza_sales;
 
